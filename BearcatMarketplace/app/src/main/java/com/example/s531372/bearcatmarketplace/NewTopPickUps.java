aaf9792e604c4706d237f04fce6ccd07370fd071
Kumar <S531372@nwmissouri.edu>
2018-10-05 18:53:46 -0500

    final new top pickup updated

---
+++ BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/NewTopPickUps.java
@@ -0,0 +1,14 @@
+package com.example.s531372.bearcatmarketplace;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NewTopPickUps extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_top_pick_ups);
+    }
+
+}
