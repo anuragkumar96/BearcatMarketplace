c73a447749418340ef958bb88fcb67f1321c7f6a
vyshnaviyreddy <S531383@nwmissouri.edu>
2018-10-05 20:13:23 -0500

    have completed shopbycategory activity

---
+++ BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/ShopByCategory.java
@@ -0,0 +1,14 @@
+package com.example.s531372.bearcatmarketplace;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ShopByCategory extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_by_category);
+    }
+
+}
--- BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/TopPickUpsActivity.java
+++ BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/TopPickUpsActivity.java
@@ -1,7 +1,9 @@
 package com.example.s531372.bearcatmarketplace;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TopPickUpsActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class TopPickUpsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_pick_ups);
+    }
+
+    public void filterFn(View v){
+
+        Intent init = new Intent(this,ShopByCategory.class);
+        startActivity(init);
     }
 }
