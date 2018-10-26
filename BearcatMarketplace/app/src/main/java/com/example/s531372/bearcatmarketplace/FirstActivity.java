b462d86d6163d94010951e1e2565e8c96a09d721
Pittu <S531378@nwmissouri.edu>
2018-10-26 12:28:21 -0500

    Added functionality for Login and SignUp Activities

--- BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/FirstActivity.java
+++ BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/FirstActivity.java
@@ -4,20 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class FirstActivity extends AppCompatActivity {
+
+    TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+
+        signup = (TextView) findViewById(R.id.signupTV);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent init = new Intent(FirstActivity.this, SignupActivity.class);
+                startActivity(init);
+            }
+        });
     }
 
-    public void signUpFun(View v){
-        Intent init = new Intent(this,SignupActivity.class);
-         startActivity(init);
 
-    }
+
 
     public void loginFn(View v){
 
--- BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/SignupActivity.java
+++ BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/SignupActivity.java
@@ -1,21 +1,91 @@
 package com.example.s531372.bearcatmarketplace;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignupActivity extends Activity {
+
+    private EditText emailET;
+    private EditText passwordET;
+
+    private TextView continueTV;
+    private TextView cancleTV;
+
+    public static Activity signup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        emailET = findViewById(R.id.emailET);
+        passwordET = findViewById(R.id.passwordET);
+
+        continueTV = findViewById(R.id.continueTV);
+        cancleTV = findViewById(R.id.cancleTV);
+
+        signup = this;
+
+
+
+        continueTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = emailET.getText().toString();
+                String passWord = passwordET.getText().toString();
+
+                if(TextUtils.isEmpty(userName)){
+                    Toast.makeText(SignupActivity.this, "Email cannot be empty", Toast.LENGTH_SHORT).show();
+                    setAlert("Email cannot be empty");
+                    return;
+                }
+                if(TextUtils.isEmpty(passWord)){
+//                    Toast.makeText(Signup.this, "Password cannot be empty", Toast.LENGTH_SHORT).show();
+                    setAlert("Password cannot be empty");
+                    return;
+                }
+                if(passWord.length()<6){
+//                    Toast.makeText(signup, "Password should be at least 6 charactors", Toast.LENGTH_SHORT).show();
+                    setAlert("Password should be at least 6 characters");
+                    return;
+                }
+                Intent i = new Intent(SignupActivity.this, FirstActivity.class);
+                i.putExtra("email", userName);
+                i.putExtra("password", passWord);
+                startActivity(i);
+            }
+        });
+
+        cancleTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
-    public void registerFn(View v){
-
-        Intent init = new Intent(this,FirstActivity.class);
-        startActivity(init);
+    void setAlert(String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message).setCancelable(false)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.setTitle("Warning..!");
+        alert.show();
     }
 }
+
+
