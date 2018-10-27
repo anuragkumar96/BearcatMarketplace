458b50dc1554b3f3985ed8d345b41a8326fa71f7
vyshnaviyreddy <S531383@nwmissouri.edu>
2018-10-26 23:57:43 -0500

    book activity added

---
+++ BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/BookActivity.java
@@ -0,0 +1,13 @@
+package com.example.s531372.bearcatmarketplace;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BookActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+    }
+}
