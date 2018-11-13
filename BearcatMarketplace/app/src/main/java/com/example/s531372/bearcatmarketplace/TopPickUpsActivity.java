2484e882f3a4d67a55fbd84d9d5960fd31dac762
Pittu <S531378@nwmissouri.edu>
2018-11-13 12:20:03 -0600

    Implemented ListView in TopPicksUpActivity

--- BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/TopPickUpsActivity.java
+++ BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/TopPickUpsActivity.java
@@ -4,42 +4,83 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class TopPickUpsActivity extends Activity {
+
+    ListView list;
+    String[] itemname ={
+            "Table",
+            "Book",
+            "Bike",
+            "Chair"
+
+    };
+
+    Integer[] imgid={
+            R.drawable.full_product_image,
+            R.drawable.kjik,
+            R.drawable.erjg,
+            R.drawable.download,
+
+    };
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_pick_ups);
+
+
+
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+//                String Slecteditem= itemname[+position];
+//                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+                if(position == 0)
+                {
+                    Intent init = new Intent(TopPickUpsActivity.this,detaildesc.class);
+                    startActivity(init);
+                }
+                else if(position == 1)
+                {
+                    Intent init = new Intent(TopPickUpsActivity.this,BookActivity.class);
+                    startActivity(init);
+                }
+
+                else if(position == 2)
+                {
+                    Intent init = new Intent(TopPickUpsActivity.this,BikeActivity.class);
+                    startActivity(init);
+                }
+
+                else if(position == 3)
+                {
+
+                    Intent init = new Intent(TopPickUpsActivity.this,ChairActivity.class);
+                    startActivity(init);
+                }
+
+            }
+        });
     }
 
-    public void filterFn(View v){
 
-        Intent init = new Intent(this,ShopByCategory.class);
-        startActivity(init);
-    }
-
-    public void tables(View v){
-
-        Intent init = new Intent(this,detaildesc.class);
-        startActivity(init);
-    }
-
-    public void chairsfn(View v){
-
-        Intent init = new Intent(this,ChairActivity.class);
-        startActivity(init);
-    }
-
-    public void booksFn(View v){
-
-        Intent init = new Intent(this,BookActivity.class);
-        startActivity(init);
-    }
-
-    public void bikesFn(View v){
-
-        Intent init = new Intent(this,BikeActivity.class);
-        startActivity(init);
-    }
 }
