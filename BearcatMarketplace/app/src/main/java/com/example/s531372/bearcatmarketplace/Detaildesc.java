d52420c368eb04fda5d6e3c1cef16f14f0658d7e
Lokeswari Pittu <s531378@nwmissouri.edu>
2018-12-05 23:16:22 -0600

    Modified Detailsdesc Activity

--- BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/Detaildesc.java
+++ BearcatMarketplace/app/src/main/java/com/example/s531372/bearcatmarketplace/Detaildesc.java
@@ -6,31 +6,41 @@ import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import Model.Order;
 
 public class Detaildesc extends Activity {
-    ImageButton btn=findViewById(R.id.imageButton5);
+    Intent intent;
+    TextView productName,Price,decription;
+    ImageView imageproduct;
+    Order data;
+    ImageButton imagecard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detaildesc);
+        intent=getIntent();
+        data= (Order) intent.getSerializableExtra("data");
+        productName=(TextView)findViewById(R.id.ProductName);
+        Price=(TextView)findViewById(R.id.Price);
+        imageproduct=(ImageView)findViewById(R.id.imageproduct);
+        imagecard=(ImageButton)findViewById(R.id.imagecard);
+        decription=(TextView)findViewById(R.id.decription);
+        decription.setText(data.getDescription());
+        imagecard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent init = new Intent(Detaildesc.this,CartActivity.class);
+                startActivity(init);
+            }
+        });
 
-
-//
-//        btn.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                new Database(getBaseContext()).addToCart(new Order());
-//
-//            }
-//        });
-
-
-
-
-
+        productName.setText(data.getProductName());
+        Price.setText("Price : $"+data.getPrice());
+        imageproduct.setBackgroundResource(data.getImage());
     }
 
 
@@ -42,7 +52,7 @@ public class Detaildesc extends Activity {
     }
 
     public void cartFn(View v){
-
+        Utils.cartList.add(data);
         Intent init = new Intent(this,CartActivity.class);
         startActivity(init);
     }
