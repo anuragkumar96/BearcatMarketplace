402ad83f7124d51dad3e305db097e5ed07df3c3b
Lokeswari Pittu <s531378@nwmissouri.edu>
2018-12-05 23:10:41 -0600

    Added ListItem Class in Models

---
+++ BearcatMarketplace/app/src/main/java/Model/ListItem.java
@@ -0,0 +1,37 @@
+package Model;
+
+public class ListItem {
+    private String name;
+    private int icon;
+
+    public boolean isChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(boolean checked) {
+        isChecked = checked;
+    }
+
+    private boolean isChecked=false;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public void setIcon(int icon) {
+        this.icon = icon;
+    }
+
+    public ListItem(String name, int icon) {
+        this.name = name;
+        this.icon = icon;
+    }
+}
